d7755d5da73581cfa92b32da818e3a601462b383
chinanf-boy <865501259@qq.com>
2018-05-12 13:12:42 +0800

    Remove utril func

--- util.js
+++ util.js
@@ -3,12 +3,3 @@ String.prototype.replaceAll = function(search, replacement) {
 
 	return target.replace(new RegExp(search, 'g'), replacement);
 };
-
-module.exports.removeHttP = function(U) {
-	let url = U;
-	url = url.slice(url.lastIndexOf('/') + 1);
-
-	url = url.split('.').join('');
-
-	return url;
-};
